2b97ff1941396c2c7c35280270685798c43ae881
Logan Mimaroglu <12798286+gluestix@users.noreply.github.com>
2022-10-21 13:20:10 -0400

    Custom CSS styling

---
+++ js/login.js
@@ -0,0 +1,33 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+'use strict'
+
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.querySelectorAll('.needs-validation')
+
+// Loop over them and prevent submission
+Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+
+        form.classList.add('was-validated');
+        //window.location = "dashboard.html";
+    }, false)
+    })
+})();
+
+function checkValidity() {
+
+}
+
+document.getElementById("reset").onclick = function () {
+    window.location = "reset.html";
+};
+
+document.getElementById("register").onclick = function () {
+    window.location = "register.html";
+};

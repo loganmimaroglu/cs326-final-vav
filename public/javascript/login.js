4a835e5fca801d6673188c3899554c8506374254
Logan Mimaroglu <12798286+gluestix@users.noreply.github.com>
2022-11-07 18:30:39 -0500

    Page refs were hard coded, breaking Heroku deployement

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,33 +1,29 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-'use strict'
+    'use strict';
 
-// Fetch all the forms we want to apply custom Bootstrap validation styles to
-var forms = document.querySelectorAll('.needs-validation')
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation');
 
-// Loop over them and prevent submission
-Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-    form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-        }
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
 
-        form.classList.add('was-validated');
-        //window.location = "dashboard.html";
-    }, false)
-    })
+                form.classList.add('was-validated');
+                //window.location = "dashboard.html";
+            }, false);
+        });
 })();
 
-function checkValidity() {
-
-}
-
-document.getElementById("reset").onclick = function () {
-    window.location = "reset.html";
+document.getElementById('reset').onclick = function () {
+    window.location = 'reset.html';
 };
 
-document.getElementById("register").onclick = function () {
-    window.location = "users/new";
+document.getElementById('register').onclick = function () {
+    window.location = 'users/new';
 };

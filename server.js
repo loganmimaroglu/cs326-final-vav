244cb1aea52b2848bacc53d9490326bbe01d614f
Logan Mimaroglu <12798286+gluestix@users.noreply.github.com>
2022-11-07 10:56:07 -0500

    Preparing for Heroku deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 const path = require('path');
 
 // serve static files
@@ -19,4 +20,4 @@ const modelRouter = require('./routes/model');
 app.use('/users', userRouter);
 app.use('/model', modelRouter);
 
-app.listen(3000);
+app.listen(port);
